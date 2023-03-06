6adf2554af33d0349988ddc2c7cebfb19a1f138a
Caleb <Calebtheeperson@gmail.com>
2023-03-06 14:33:49 -0800

    Fixed loading cirlce bug and started on backend

--- FrontEnd/trackt/src/components /LoadingCircle.js
+++ FrontEnd/trackt/src/components /LoadingCircle.js
@@ -1,7 +1,6 @@
 import '../style/LoadingCircle.css'
 
 export default function loadingCircle(props){
-    const isLoading = props.loading
 
     return(
         <div className="loader"></div>
--- FrontEnd/trackt/src/pages/Home.js
+++ FrontEnd/trackt/src/pages/Home.js
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import '../style/Home.css'
+import axios from 'axios'
+require('cors')
 
 
 export default function Home(){
@@ -10,6 +12,7 @@ export default function Home(){
     const [link, updateLink] = useState("")
     const [submit, updateSubmit] = useState(false)
     const [loadingCircle, updateStatus] = useState(false)
+    const [appropriateLink, updateLinkStatus] = useState(true)
 
     //OnChange Methods
     const changeLink = (event, method) => {
@@ -18,12 +21,23 @@ export default function Home(){
 
     const changeStatus = (event, method) => {
         // updateSubmit(true)
-        updateStatus(true)
-        console.log("stats changed")
-        console.log(loadingCircle)
+
+        if (!link.includes("linkedin") && !link.includes("Linkedin")) {
+            updateLinkStatus(false)
+        } else {
+            updateStatus(true)
+            console.log("stats changed")
+            console.log(loadingCircle)
+            getInformation()
+        }
+
     }
 
-    //API Function will go here 
+    //API Function will go here
+    
+    async function getInformation() {
+        await axios(`http://localhost:3001/scrapping?url=`+encodeURI(link) )
+    }
 
 
     //The view that is being rendered
@@ -36,11 +50,12 @@ export default function Home(){
             <body id = 'mainBody'>
                 <div id = "mainContent">
                 <div id = "contentWrapper">
-                    <h1 className = "mainText">Linkdin Profile Scraper</h1>
+                    <h1 className = "mainText">Linkedin Profile Scraper</h1>
                     <div className = 'formHolder'>
                     <Form>
+                    {!appropriateLink && <Form.Label>Make sure the url is a Linkedin url</Form.Label>}
                         <Form.Group className = "mb-3">
-                            <Form.Control className = "emailField" type="email" placeholder="Enter URL of Linkdin Profile" onChange={changeLink}></Form.Control>
+                            <Form.Control className = "emailField" placeholder="Enter URL of Linkdin Profile" onChange={changeLink}></Form.Control>
                         </Form.Group>
                         <Button id = "submitButton" variant="primary" type="submit" onClick={changeStatus}>Submit</Button>
                         { loadingCircle && <LoadingCircle />}
